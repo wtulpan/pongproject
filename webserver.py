d8735300961f952fb71eb429682fd8589c2437ca
William Tulpan <william.tulpan@datadocksolutions.com>
2020-02-27 18:15:13 -0500

    Initial working commit to use as a template

---
+++ webserver.py
@@ -0,0 +1,23 @@
+import tornado.ioloop
+import tornado.web
+
+class MainHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.render("landing.html")
+
+
+class GameHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.render("game.html")
+
+
+def make_app():
+    return tornado.web.Application([
+        (r"/", MainHandler),
+	(r"/game", GameHandler),
+    ])
+
+if __name__ == "__main__":
+    app = make_app()
+    app.listen(8888)
+    tornado.ioloop.IOLoop.current().start()
